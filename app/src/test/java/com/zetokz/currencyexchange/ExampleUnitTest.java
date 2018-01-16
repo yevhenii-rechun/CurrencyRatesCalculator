da31086d0d1ea66329d575d7250daf5ad29b1465
zetokz <iamzetok@gmail.com>
2018-01-16 21:02:59 +0200

    - First commit

---
+++ app/src/test/java/com/zetokz/currencyexchange/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package com.zetokz.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
